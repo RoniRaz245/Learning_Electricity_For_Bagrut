940d4677458bdaf5037e160015cecb5a715c1024
Roni <roniapollo1@gmail.com>
2023-10-30 16:50:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/learningelectricityforbagrut/LoginActivity.java
@@ -0,0 +1,99 @@
+package com.example.learningelectricityforbagrut;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.Button;
+import android.text.TextUtils;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailTextView, passwordTextView;
+
+    private final Button TakeToSignUp = findViewById(R.id.TakeToSignUp);
+
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // taking instance of FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
+
+        // initialising all views through id defined above
+        emailTextView = findViewById(R.id.email);
+        passwordTextView = findViewById(R.id.password);
+        Button login = findViewById(R.id.login);
+
+        // Set on Click Listener on Sign-in button
+        login.setOnClickListener(v -> loginUserAccount());
+    }
+
+    private void loginUserAccount()
+    {
+
+        // Take the value of two edit texts in Strings
+        String email, password;
+        email = emailTextView.getText().toString();
+        password = passwordTextView.getText().toString();
+
+        // validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter email!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter password!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(
+                        new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(
+                                    @NonNull Task<AuthResult> task)
+                            {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login successful!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
+
+                                    // if sign-in is successful
+                                    // intent to home activity
+                                    Intent intent
+                                            = new Intent(LoginActivity.this,
+                                            MainActivity.class);
+                                    startActivity(intent);
+                                }
+
+                                else {
+
+                                    // sign-in failed
+                                    Toast.makeText(getApplicationContext(),
+                                                    "Login failed!!",
+                                                    Toast.LENGTH_LONG)
+                                            .show();
+
+                                }
+                            }
+                        });
+                        TakeToSignUp.setOnClickListener(v -> startActivity(new Intent(LoginActivity.this, MainActivity.class)));
+    }
+}
