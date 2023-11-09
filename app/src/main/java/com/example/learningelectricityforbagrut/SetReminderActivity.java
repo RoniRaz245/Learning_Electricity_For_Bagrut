46ce10643ba267024cc3a17ab0fd24120eb51268
Roni <roniapollo1@gmail.com>
2023-11-09 17:25:23 +0200

    notification maker written- not debugged yet

--- app/src/main/java/com/example/learningelectricityforbagrut/SetReminderActivity.java
+++ app/src/main/java/com/example/learningelectricityforbagrut/SetReminderActivity.java
@@ -1,6 +1,5 @@
 package com.example.learningelectricityforbagrut;
 
-import static android.view.View.*;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +9,7 @@ import android.app.AlarmManager;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.content.Context;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.os.Build;
 import android.view.View;
@@ -19,10 +18,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
 import android.os.Bundle;
-import androidx.core.app.NotificationCompat;
 import android.app.Notification;
 import android.widget.Toast;
-
+import android.content.Context;
+import androidx.core.app.NotificationManagerCompat;
 import java.util.Calendar;
 
 public class SetReminderActivity extends AppCompatActivity {
@@ -30,6 +29,9 @@ public class SetReminderActivity extends AppCompatActivity {
     EditText textForNotif;
     Button makeNotif;
     ImageButton homeButton;
+    int id=0;
+    static public int currentid;
+    public static String channelid;
 
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -37,33 +39,36 @@ public class SetReminderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_reminder);
-        timePicker=findViewById(R.id.timePicker);
-        textForNotif=findViewById(R.id.textForNotif);
-        makeNotif=findViewById(R.id.makeNotif);
-        homeButton=findViewById(R.id.goHome);
+        timePicker = findViewById(R.id.timePicker);
+        textForNotif = findViewById(R.id.textForNotif);
+        makeNotif = findViewById(R.id.makeNotif);
+        homeButton = findViewById(R.id.goHome);
         homeButton.setOnClickListener(v -> homeButton.getContext().startActivity(new Intent(homeButton.getContext(), HomeActivity.class)));
-        makeNotif.setOnClickListener(v -> createNotif());
-    }
-    @SuppressLint("ScheduleExactAlarm")
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void createNotif(){
-        String CHANNEL_ID="MYCHANNEL";
-        NotificationChannel notificationChannel=new NotificationChannel(CHANNEL_ID,"name",NotificationManager.IMPORTANCE_LOW);
-        Notification notification=new Notification.Builder(getApplicationContext(),CHANNEL_ID)
-                .setContentText(textForNotif.getText())
-                .setContentTitle(getResources().getString(R.string.app_name_hebrew))
-                .setChannelId(CHANNEL_ID)
-                .setSmallIcon(R.drawable.electricity_icon)
-                .build();
-
-        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        Intent intent = new Intent(getApplicationContext(), Notification.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 1, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.HOUR_OF_DAY, timePicker.getHour());
-        calendar.set(Calendar.MINUTE, timePicker.getMinute());
-        alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
-        Toast.makeText(getApplicationContext(), "התראה נקבעה", Toast.LENGTH_LONG).show();
+        makeNotif.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ScheduleExactAlarm")
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                createNotificationChannel();
+                int currentid=id;
+                AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+                Intent intent = new Intent();
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 1, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+                alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+                id++;
+                Toast.makeText(getApplicationContext(), "Scheduled ", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= 26) {
+            channelid = "channelID";
+            String name = "Alerts";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(channelid, name, importance);
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            NotificationManagerCompat.from(this).createNotificationChannel(channel);
+        }
+    }
 }
