a586ea3b99690a2164357d292626b45cc1d53f94
Roni <roniapollo1@gmail.com>
2023-12-05 21:46:49 +0200

    finished setting up TTS

--- app/src/main/java/com/example/learningelectricityforbagrut/QuestionViewActivity.java
+++ app/src/main/java/com/example/learningelectricityforbagrut/QuestionViewActivity.java
@@ -2,20 +2,40 @@ package com.example.learningelectricityforbagrut;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.Locale;
 
 public class QuestionViewActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
     protected TextToSpeech tts;
+    protected TextView textView; //temporary text to check TTS
+    protected Button ttsButton;
+    protected String text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_view);
+        textView=findViewById(R.id.textView);
+        ttsButton=findViewById(R.id.ttsButton);
 
         TextToSpeech tts= new TextToSpeech(this.getApplicationContext(),this);
+
+        ttsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textToSpeak(); //calls function that reads out text
+            }
+        });
+    }
+    private void textToSpeak(){
+        text=textView.getText().toString();
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
     }
 
     @Override
@@ -24,16 +44,12 @@ public class QuestionViewActivity extends AppCompatActivity implements TextToSpe
             Locale Hebrew= new Locale("he");
             int result= tts.setLanguage(Hebrew);
             if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                //if tts is initialized but hebrew isn't supported
                 Log.e("error", "This Language is not supported");
-            } else {
-                textToSpeak();
             }
         }
         else {
             Log.e("error", "Failed to Initialize");
         }
-    }
-    private void textToSpeak(){
-
     }
 }
