103763d26dfb241c000e41b18605b4bbbb807ad9
Roni <roniapollo1@gmail.com>
2023-12-07 14:01:27 +0200

    continuing to implement pick image functionality

--- app/src/main/java/com/example/learningelectricityforbagrut/MakeQuestionActivity.java
+++ app/src/main/java/com/example/learningelectricityforbagrut/MakeQuestionActivity.java
@@ -57,13 +57,7 @@ public class MakeQuestionActivity extends AppCompatActivity {
         String uploadedImage = UUID.randomUUID().toString(); //generating unique ID for image
         StorageReference imageRef = storageRef.child("images/"+uploadedImage);
 
-        try{
-            Intent i = new Intent(Intent.ACTION_PICK,
-                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(i, RESULT_LOAD_IMAGE);
-        }catch(Exception exp){
-            Log.i("Error",exp.toString());
-        }
+        
 
 
     }
