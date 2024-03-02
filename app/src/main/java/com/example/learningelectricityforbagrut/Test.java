87cac9ebe92ff05dc8d60c543cc638a4f6d2ca14
Roni <roniapollo1@gmail.com>
2024-03-02 16:45:20 +0200

    added constructors, getters and testers to Test.java

--- app/src/main/java/com/example/learningelectricityforbagrut/Test.java
+++ app/src/main/java/com/example/learningelectricityforbagrut/Test.java
@@ -6,4 +6,60 @@ public class Test {
     boolean[] correctAnswerGiven;
     int[] timers;
     String timeTaken;
+    //constructors
+    public Test(){
+        this.UID=null;
+        this.questions=null;
+        this.correctAnswerGiven=null;
+        this.timers=null;
+        this.timeTaken=null;
+    }
+    public Test(String _UID, Question[] _questions, boolean[] _correctAnswerGiven, int[] _timers, String _timeTaken){
+        this.UID=_UID;
+        this.questions=_questions;
+        this.correctAnswerGiven=_correctAnswerGiven;
+        this.timers=_timers;
+        this.timeTaken=_timeTaken;
+
+    }
+    //getting and setting functions
+    public boolean[] getCorrectAnswerGiven() {
+        return correctAnswerGiven;
+    }
+
+    public int[] getTimers() {
+        return timers;
+    }
+
+    public Question[] getQuestions() {
+        return questions;
+    }
+
+    public String getTimeTaken() {
+        return timeTaken;
+    }
+
+    public String getUID() {
+        return UID;
+    }
+
+    public void setCorrectAnswerGiven(boolean[] correctAnswerGiven) {
+        this.correctAnswerGiven = correctAnswerGiven;
+    }
+
+    public void setQuestions(Question[] questions) {
+        this.questions = questions;
+    }
+
+    public void setTimers(int[] timers) {
+        this.timers = timers;
+    }
+
+    public void setTimeTaken(String timeTaken) {
+        this.timeTaken = timeTaken;
+    }
+
+    public void setUID(String UID) {
+        this.UID = UID;
+    }
 }
