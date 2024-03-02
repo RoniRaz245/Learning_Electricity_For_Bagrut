e7d8bd6b641135e8bed6a4607196bc7f7807b977
Roni <roniapollo1@gmail.com>
2024-03-02 16:36:39 +0200

    defined Test class

---
+++ app/src/main/java/com/example/learningelectricityforbagrut/Test.java
@@ -0,0 +1,9 @@
+package com.example.learningelectricityforbagrut;
+
+public class Test {
+    String UID;
+    Question[] questions;
+    boolean[] correctAnswerGiven;
+    int[] timers;
+    String timeTaken;
+}
