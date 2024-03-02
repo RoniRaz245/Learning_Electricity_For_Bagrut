d87b0e7cd70c01455b9188f66a8dd4c017e4eb5d
Roni <roniapollo1@gmail.com>
2024-03-02 16:55:39 +0200

    added empty constructor and getters&setters to Question.java

--- app/src/main/java/com/example/learningelectricityforbagrut/Question.java
+++ app/src/main/java/com/example/learningelectricityforbagrut/Question.java
@@ -13,9 +13,17 @@ public class Question {
     private ArrayList<Double> times;
     private String teacher;
     private long serialNumber;
-
-
-
+    //constructors
+    public Question(){
+        this.questionBody=null;
+        this.questionLevel=0;
+        this.imageUrl=null;
+        this.answers=null;
+        this.correctAnswer=0;
+        this.times= null;
+        this.teacher=null;
+        this.serialNumber=0;
+    }
     public Question(String _questionBody, String _image, String[] _answers, int _correctAnswer, int _questionLevel, String UID, long num)
     {
         this.questionBody=_questionBody;
@@ -26,5 +34,70 @@ public class Question {
         this.times= new ArrayList<Double>();
         this.teacher=UID;
         this.serialNumber=num;
+    }
+    
+    //getting and setting functions
+    public int getQuestionLevel() {
+        return questionLevel;
+    }
+
+    public ArrayList<Double> getTimes() {
+        return times;
+    }
+
+    public int getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public String getQuestionBody() {
+        return questionBody;
+    }
+
+    public long getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public String[] getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(String[] answers) {
+        this.answers = answers;
+    }
+
+    public void setCorrectAnswer(int correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public void setQuestionBody(String questionBody) {
+        this.questionBody = questionBody;
+    }
+
+    public void setQuestionLevel(int questionLevel) {
+        this.questionLevel = questionLevel;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public void setSerialNumber(long serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public void setTimes(ArrayList<Double> times) {
+        this.times = times;
     }
 }
