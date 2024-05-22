47aa81e75e70877c7ee1db42115daf44fa3501de
Roni <roniapollo1@gmail.com>
2024-05-22 13:48:21 +0300

    fitted serial number to change in amounts of questions on firebase

--- app/src/main/java/com/example/learningelectricityforbagrut/MakeQuestionActivity.java
+++ app/src/main/java/com/example/learningelectricityforbagrut/MakeQuestionActivity.java
@@ -198,7 +198,7 @@ public class MakeQuestionActivity extends baseActivity {
                 if(!task.isSuccessful())
                     Toast.makeText(MakeQuestionActivity.this, "הייתה שגיאה, אנא נסו שוב", Toast.LENGTH_SHORT).show();
                 else{
-                    long serialNum = task.getResult().child("level_1").getChildrenCount() + task.getResult().child("level_2").getChildrenCount() + task.getResult().child("level_3").getChildrenCount() + task.getResult().child("level_4").getChildrenCount()+task.getResult().child("level_5").getChildrenCount()+task.getResult().child("deleted_questions").getChildrenCount()-5;
+                    long serialNum = task.getResult().child("level_1").getChildrenCount() + task.getResult().child("level_2").getChildrenCount() + task.getResult().child("level_3").getChildrenCount() + task.getResult().child("level_4").getChildrenCount()+task.getResult().child("level_5").getChildrenCount()+task.getResult().child("deleted_questions").getChildrenCount()-4;
                     String UID = mAuth.getCurrentUser().getUid();
                     String image = "0";
                     if (imageUploaded)
