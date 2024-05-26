155a1532671ca069873e837e62a1c478d789396d
Roni <roniapollo1@gmail.com>
2024-05-26 19:28:31 +0300

    building firestore query for recent tests to decide if to change user level

--- app/src/main/java/com/example/learningelectricityforbagrut/User.java
+++ app/src/main/java/com/example/learningelectricityforbagrut/User.java
@@ -1,5 +1,15 @@
 package com.example.learningelectricityforbagrut;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class User {
     private int level;
     private boolean isTeacher;
@@ -48,8 +58,21 @@ public class User {
     public long getLicense() { return license; }
     public int getPhoneNum() { return phoneNum; }
 
-    public void updateLevel(double grade){
-        //if user got low grade take him down levels, if got a high grade take up levels
+    public void updateLevel(){
+        //if user consistently gets low grades take him down levels, if consistently gets high grades take up levels
         //TODO: decide on grade ranges later
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("tests").whereEqualTo("UID", this.getUID()).orderBy("timeTaken", Query.Direction.DESCENDING).limit(10).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+
+                    }
+                }
+            }
+        });
     }
 }
