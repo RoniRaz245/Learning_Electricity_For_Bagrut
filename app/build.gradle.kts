af785a1574d598487e117bd67360b0a563e1a82d
Roni <roniapollo1@gmail.com>
2024-05-27 16:45:22 +0300

    updated compileSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.learningelectricityforbagrut"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.learningelectricityforbagrut"
@@ -38,11 +38,11 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation ("androidx.activity:activity:1.7.2")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation ("androidx.activity:activity:1.9.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("androidx.preference:preference-ktx:1.2.0")
-    implementation ("com.google.android.material:material:1.1.0")
+    implementation ("androidx.preference:preference-ktx:1.2.1")
+    implementation ("com.google.android.material:material:1.12.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.8.10")
     implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
@@ -51,9 +51,9 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-storage")
     implementation("androidx.preference:preference:1.2.1")
-    implementation("androidx.fragment:fragment:1.6.2")
-    implementation("androidx.navigation:navigation-fragment:2.6.0")
-    implementation("androidx.navigation:navigation-ui:2.6.0")
+    implementation("androidx.fragment:fragment:1.7.1")
+    implementation("androidx.navigation:navigation-fragment:2.7.7")
+    implementation("androidx.navigation:navigation-ui:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
