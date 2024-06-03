37dbd184352946c434368f4ef2f06463f356ccf8
Roni <roniapollo1@gmail.com>
2024-06-03 21:41:29 +0300

    working on getting rid of hardcoded strings

--- app/src/main/java/com/example/learningelectricityforbagrut/imageSourceFragment.java
+++ app/src/main/java/com/example/learningelectricityforbagrut/imageSourceFragment.java
@@ -13,19 +13,19 @@ public class imageSourceFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
-        builder.setTitle("מקור התמונה");
-        builder.setPositiveButton("גלריה", new DialogInterface.OnClickListener(){
+        builder.setTitle(getString(R.string.image_source));
+        builder.setPositiveButton(getString(R.string.gallery), new DialogInterface.OnClickListener(){
             @Override
             public void onClick(DialogInterface dialog, int which){
-                result.putCharSequence("source","gallery");
-                getParentFragmentManager().setFragmentResult("image source",result);
+                result.putCharSequence(getString(R.string.source),getString(R.string.gallery_english));
+                getParentFragmentManager().setFragmentResult(getString(R.string.image_source_english),result);
             }
         });
-        builder.setNeutralButton("מצלמה", new DialogInterface.OnClickListener() {
+        builder.setNeutralButton(getString(R.string.camera), new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                result.putCharSequence("source","camera");
-                getParentFragmentManager().setFragmentResult("image source",result);
+                result.putCharSequence(getString(R.string.source),getString(R.string.camera_english));
+                getParentFragmentManager().setFragmentResult(getString(R.string.image_source_english),result);
             }
         });
         builder.setCancelable(true);
