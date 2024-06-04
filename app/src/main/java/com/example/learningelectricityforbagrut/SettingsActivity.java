5af6c048949d40e491a7ae1c235fa3e57538a18b
Roni <roniapollo1@gmail.com>
2024-06-04 13:44:17 +0300

    added logout capability

--- app/src/main/java/com/example/learningelectricityforbagrut/SettingsActivity.java
+++ app/src/main/java/com/example/learningelectricityforbagrut/SettingsActivity.java
@@ -1,10 +1,18 @@
 package com.example.learningelectricityforbagrut;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsActivity extends baseActivity {
+    Button signOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +21,23 @@ public class SettingsActivity extends baseActivity {
 
         setSupportActionBar(findViewById(R.id.my_toolbar));
         getSupportActionBar().show();
+
+        signOut=findViewById(R.id.logout);
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //clear sharedPreferences data
+                SharedPreferences prefs= PreferenceManager.getDefaultSharedPreferences(signOut.getContext());
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.clear();
+                editor.commit();
+                //log out through firebase auth
+                FirebaseAuth.getInstance().signOut();
+                Intent loginScreen=new Intent(signOut.getContext(), LoginActivity.class);
+                signOut.getContext().startActivity(loginScreen);
+                finish();
+            }
+        });
 
         getSupportFragmentManager()
                 .beginTransaction()
